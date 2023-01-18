99ce1d03f68ce955bc7ea05c90d7255a109ca9e9
KhalilAhmadMughal <58709039+KhalilAhmadMughal@users.noreply.github.com>
2023-01-18 18:01:34 +0500

    Add files via upload

--- customer/app.js
+++ customer/app.js
@@ -1,53 +1,55 @@
 const api_url = "http://localhost:3000/customers";
+const customerContainer = document.getElementById("customer-container");
+
 async function getApiData(url) {
   const response = await fetch(url);
   var data = await response.json();
-  show(data);
+  showCustomerProfile(data);
 }
-getApiData(api_url);
-let customerContainer = document.getElementById("customer-container");
 
-function show(data) {
+function showCustomerProfile(data) {
   for (obj of data) {
     let customer = document.createElement("div");
     customer.innerHTML = obj.name;
     customer.id = obj.id;
     customer.classList = "customer";
-
     customer.addEventListener("click", function () {
-      showDetails(customer.id, data);
+      showCustomerDetails(customer.id, data);
     });
     customerContainer.appendChild(customer);
   }
 }
 
-function showDetails(id, data) {
+function showCustomerDetails(id, data) {
   let obj = data[id - 1];
   let len = Object.keys(obj).length;
   const detailContainer = document.getElementById("detail-box");
-  detailContainer.innerHTML = "";
-
   let heading = document.createElement("h2");
+
+  detailContainer.innerHTML = "";
   heading.innerHTML = "Customer- " + obj.id + " -Details";
   detailContainer.appendChild(heading);
+  showObjectDetail(obj, len, detailContainer);
 
-  for (let i = 0; i < len; i++) {
-    let section = document.createElement("div");
-    section.classList = "section";
-    for (let j = 0; j < 2; j++) {
-      if (j === 0) {
-        let title = document.createElement("p");
-        title.classList = "title";
-        title.innerHTML = Object.keys(obj)[i];
-        section.appendChild(title);
-      } else {
-        let content = document.createElement("p");
-        content.classList = "content";
-        content.innerHTML = obj[Object.keys(obj)[i]];
-        section.appendChild(content);
-      }
-    }
-    detailContainer.appendChild(section);
-  }
   detailContainer.style.display = "block";
 }
+
+async function getDetailSectionComponent() {
+  const response = await fetch("/detailSection.html");
+  let sectionData = await response.text();
+  return sectionData;
+}
+
+async function showObjectDetail(obj, len, detailContainer) {
+  let newSection = await getDetailSectionComponent();
+  let html;
+  for (let index = 0; index < len; index++) {
+    html = newSection
+      .replace("$title", Object.keys(obj)[index])
+      .replace("$content", obj[Object.keys(obj)[index]]);
+    detailContainer.innerHTML += html;
+  }
+}
+
+// Function Calls
+getApiData(api_url);
